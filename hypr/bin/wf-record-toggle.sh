7768ad793fe9c5656cb75e2da0838fba964637dd
looperecho <luggage_camphor.0w@icloud.com>
2025-03-05 06:26:46 +0000

    script to toggle screen recording

---
+++ hypr/bin/wf-record-toggle.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Toggle Script for wf-recorder
+
+PID_FILE="/tmp/wf-record.pid"
+OUTPUT_FILE="$HOME/Pictures/Screenshots/screen_record_$(date +'%Y-%m-%d_%H-%M-%S').mp4"
+
+if [[ -f "$PID_FILE" ]]; then
+    # wf-recorder is running, so stop it
+    kill "$(cat "$PID_FILE")"
+    rm "$PID_FILE"
+    
+    notify-send \
+        "Recording Stopped" \
+        "Saved to Screenshots folder." \
+        -i video-x-generic
+else
+    # Start wf-recorder
+    wf-recorder \
+        -g "$(slurp)" \
+        -f "$OUTPUT_FILE" \
+        & echo $! > "$PID_FILE"
+    
+    notify-send \
+        "Screen Recording" \
+        "Recording started..." \
+        -i video-x-generic \
+        -u critical
+fi
