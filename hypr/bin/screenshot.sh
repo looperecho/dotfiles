6346aea8fbd9e46ac31221c6149f15b53cffb269
looperecho <luggage_camphor.0w@icloud.com>
2025-03-16 22:26:42 +0000

    added grim, slurp and swappy as screenshot method

---
+++ hypr/bin/screenshot.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Screenshot with grim, slurp and swappy
+#
+
+file_path="$HOME/Pictures/Screenshots/$(date +"%Y-%m-%d_%H-%M").png"
+file_name=$(basename ${file_path})
+
+# Function to notify after swappy
+swappy_notify() {
+    if [ -f $file_path ]; then
+        notify-send -u normal
+            -i $file_path \
+            "Screenshot" \
+            "Saved as: \n$file_name"
+
+    elif [[ $(wl-paste -l) == "image/png" ]]; then
+        notify-send -u low \
+            -i desktop-symbolic \
+            -h boolean:transient:true \
+            "Screenshot" \
+            "Copied to clipboard"
+    fi
+}
+
+# Screenshot action
+grim -g "$(slurp -d)" - | swappy -f - && swappy_notify
