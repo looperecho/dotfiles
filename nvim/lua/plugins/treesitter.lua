1e009058b5ea3d6da46a50455f8a4012beda1cf3
looperecho <luggage_camphor.0w@icloud.com>
2025-04-24 07:46:56 +0100

    comments

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,15 @@
--- plugins/treesitter.lua
 -- better syntax highlighting
+-- plugins/treesitter.lua
+
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = { "lua", "python", "javascript", "html", "css" },
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = { enable = true },
-    }
-  end,
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+        require('nvim-treesitter.configs').setup {
+            ensure_installed = { "lua", "python", "javascript", "html", "css" },
+            highlight = { enable = true },
+            indent = { enable = true },
+            incremental_selection = { enable = true },
+        }
+    end,
 }
