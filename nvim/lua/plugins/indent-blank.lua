6153830f1cdbbfca710440608a7cef6a2f84bb6a
looperecho <luggage_camphor.0w@icloud.com>
2025-04-24 07:46:42 +0100

    comments

--- nvim/lua/plugins/indent-blank.lua
+++ nvim/lua/plugins/indent-blank.lua
@@ -1,11 +1,13 @@
 -- Add indentation guides even on blank lines
+-- plugins/indent-blank.lua
+
 return {
     {
         'lukas-reineke/indent-blankline.nvim',
         main = 'ibl',
         opts = {
             indent = {
-                char = "▏" 
+                char = "▏"
             },
             scope = {
                 show_start = false,
