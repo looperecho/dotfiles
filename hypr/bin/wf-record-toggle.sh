49bdea49b6eb11c33c59ab9a4cab30ab9ef77b4c
looperecho <luggage_camphor.0w@icloud.com>
2025-06-12 10:25:37 +0100

    modified flag for waybar icon

--- hypr/bin/wf-record-toggle.sh
+++ hypr/bin/wf-record-toggle.sh
@@ -2,12 +2,14 @@
 # Toggle Script for wf-recorder
 
 PID_FILE="/tmp/wf-record.pid"
+FLAG_FILE="/tmp/.wf-recording"
 OUTPUT_FILE="$HOME/Pictures/Captures/$(date +'%Y-%m-%d_%H-%M').mp4"
 
 if [[ -f "$PID_FILE" ]]; then
     # wf-recorder is running, so stop it
     kill "$(cat "$PID_FILE")"
     rm "$PID_FILE"
+    rm -f "$FLAG_FILE"
     
     notify-send \
         "Recording Stopped" \
@@ -20,7 +22,8 @@ else
         -g "$(slurp)" \
         -f "$OUTPUT_FILE" \
         & echo $! > "$PID_FILE"
-    
+    touch "$FLAG_FILE"     
+
     notify-send \
         "Screen Recording" \
         "Recording started..." \
