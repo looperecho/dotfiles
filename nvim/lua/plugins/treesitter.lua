b6095b98177f9861b2a31bba107d18dc5d4146b0
looperecho <luggage_camphor.0w@icloud.com>
2026-01-31 19:48:20 +0000

    refactor to address stale config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,22 +2,25 @@
 -- plugins/treesitter.lua
 
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-        require('nvim-treesitter.configs').setup {
-            ensure_installed = {
-                "lua",
-                "python",
-                "javascript",
-                "typescript",
-                "tsx",
-                "html",
-                "css"
-            },
-            highlight = { enable = true },
-            indent = { enable = true },
-            incremental_selection = { enable = true },
-        }
-    end,
+    {
+        "nvim-treesitter/nvim-treesitter",
+        name = "nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.config").setup({
+                ensure_installed = {
+                    "lua",
+                    "python",
+                    "javascript",
+                    "typescript",
+                    "tsx",
+                    "html",
+                    "css",
+                },
+                highlight = { enable = true },
+                indent = { enable = true },
+                incremental_selection = { enable = true },
+            })
+        end,
+    },
 }
