f4d26f464979c0c37874fa1cb0286a7649a8a219
looperecho <luggage_camphor.0w@icloud.com>
2026-01-31 19:48:13 +0000

    refactor to address stale config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,85 +2,99 @@
 -- plugins/lsp.lua
 
 return {
-    {
-        'neovim/nvim-lspconfig',
-        dependencies = {
-            'williamboman/mason.nvim',
-            'williamboman/mason-lspconfig.nvim',
-        },
-        config = function()
-            -- Mason
-            require('mason').setup()
-            require('mason-lspconfig').setup {
-                ensure_installed = {
-                    "jedi_language_server", -- Python
-                    "cssls",                -- CSS
-                    "bashls",               -- Bash
-                    "jsonls",               -- JSON
-                    "yamlls",               -- YAML
-                    "lua_ls",               -- Lua
-                },
-                automatic_installation = true,
-            }
+  -- Mason core
+  {
+    "williamboman/mason.nvim",
+    opts = {},
+  },
 
-            local lspconfig = require('lspconfig')
-
-            -- LSP capabilities
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            local has_cmp, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
-            if has_cmp then
-                capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
-            end
-
-            -- Keymaps
-            local on_attach = function(client, bufnr)
-                client.server_capabilities.semanticTokensProvider = nil
-                local map = function(mode, lhs, rhs, desc)
-                    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-                end
-                map('n', 'K', vim.lsp.buf.hover, "Hover info")
-                map('n', '<leader>gd', vim.lsp.buf.definition, "Go to definition")
-                map('n', '<leader>gr', vim.lsp.buf.references, "Go to references")
-                map('n', '<leader>rn', vim.lsp.buf.rename, "Rename symbol")
-                map('n', '<leader>ca', vim.lsp.buf.code_action, "Code action")
-                map('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, "Format file")
-            end
-
-            -- Server-specific configs
-            local servers = {
-                -- Python
-                jedi_language_server = {
-                    settings = {
-                        jedi = {
-                            completion = {
-                                resolveEagerly = true,
-                            },
-                            diagnostics = {
-                                enable = true,
-                            },
-                        },
-                    },
-                },
-                cssls = {},
-                bashls = {},
-                jsonls = {},
-                yamlls = {},
-                -- Lua
-                lua_ls = {
-                    settings = {
-                        Lua = {
-                            diagnostics = { globals = { 'vim' } },
-                            workspace = { checkThirdParty = false },
-                        },
-                    },
-                },
-            }
-
-            for server, config in pairs(servers) do
-                config.on_attach = on_attach
-                config.capabilities = capabilities
-                lspconfig[server].setup(config)
-            end
-        end,
+  -- Mason <-> LSP bridge (installs + can auto-enable)
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
     },
+    opts = {
+      ensure_installed = {
+        "jedi_language_server",
+        "cssls",
+        "bashls",
+        "jsonls",
+        "yamlls",
+        "lua_ls",
+      },
+
+      -- We'll explicitly enable servers ourselves (below),
+      -- so turn off mason-lspconfig's automatic enabling.
+      automatic_enable = false,
+    },
+  },
+
+  -- Server configs live in nvim-lspconfig, but setup() is deprecated.
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      -- LSP capabilities (optionally extended by cmp-nvim-lsp)
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      if ok then
+        capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+      end
+
+      -- Keymaps + buffer-local attach behavior
+      local on_attach = function(client, bufnr)
+        -- disable semantic tokens if you don't like the highlights
+        client.server_capabilities.semanticTokensProvider = nil
+
+        local map = function(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+        end
+
+        map("n", "K", vim.lsp.buf.hover, "Hover info")
+        map("n", "<leader>gd", vim.lsp.buf.definition, "Go to definition")
+        map("n", "<leader>gr", vim.lsp.buf.references, "Go to references")
+        map("n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+        map("n", "<leader>ca", vim.lsp.buf.code_action, "Code action")
+        map("n", "<leader>f", function()
+          vim.lsp.buf.format({ async = true })
+        end, "Format file")
+      end
+
+      -- Server-specific configs (same idea as before, just no .setup())
+      local servers = {
+        jedi_language_server = {
+          settings = {
+            jedi = {
+              completion = { resolveEagerly = true },
+              diagnostics = { enable = true },
+            },
+          },
+        },
+        cssls = {},
+        bashls = {},
+        jsonls = {},
+        yamlls = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = { globals = { "vim" } },
+              workspace = { checkThirdParty = false },
+            },
+          },
+        },
+      }
+
+      -- Define/extend configs with vim.lsp.config()
+      for name, cfg in pairs(servers) do
+        cfg.capabilities = capabilities
+        cfg.on_attach = on_attach
+        vim.lsp.config(name, cfg)
+      end
+
+      -- Enable them so they attach to their filetypes
+      -- (In newer 0.11.x, enabling will also attach to already-open buffers.)
+      vim.lsp.enable(vim.tbl_keys(servers))
+    end,
+  },
 }
