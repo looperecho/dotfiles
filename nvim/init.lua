cdd290991f9a40f70691b5168a25d7e095465053
looperecho <luggage_camphor.0w@icloud.com>
2026-04-07 11:20:16 +0100

    move to nvim 12

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,27 +1,21 @@
--- neovim entry point
--- ~/.config/init.lua
+-- nvim 12 config
 
--- load leymaps
-require("system.keymaps")
+require('keybinds')
 
--- load lazy
-require("system.lazy")
+-- misc
+vim.opt.termguicolors = true
 
 -- set title
 vim.o.title = true
 vim.o.titlestring = "Neovim - %{expand('%:t')}"
 
--- theme
-vim.cmd.colorscheme("hybrid")
+-- Sync clipboard
+vim.opt.clipboard = 'unnamedplus'
 
--- enable nerdfont
-vim.g.have_nerd_font = true
-
--- Line Numbers
+-- Lines
 vim.opt.number = true
 vim.opt.relativenumber = true
-
--- Cursor line indicator
+vim.opt.scrolloff = 12
 vim.opt.cursorline = true
 
 -- Tab indentation
@@ -31,35 +25,73 @@ vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 vim.opt.expandtab = true
 
+-- Faster vim
+vim.opt.updatetime = 50
+vim.opt.timeoutlen = 300
+
 -- Incremental search highlighting
 vim.opt.incsearch = true
 vim.opt.hlsearch = false
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Sync clipboard
-vim.opt.clipboard = 'unnamedplus'
-
--- Faster vim
-vim.opt.updatetime = 50
-
--- Display which-key faster
-vim.opt.timeoutlen = 300
-
--- Scrolloff
-vim.opt.scrolloff = 12
-
--- Good colors
-vim.opt.termguicolors = true
-
 -- Highlight when yanking
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+  callback = function() vim.highlight.on_yank() end,
 })
 
 -- Disable swap
 vim.opt.swapfile = false
 vim.opt.backup = false
+
+-- new plugin manager
+vim.pack.add { 
+	'https://github.com/neovim/nvim-lspconfig',
+	'https://github.com/HoNamDuong/hybrid.nvim',
+	'https://github.com/rebelot/kanagawa.nvim',
+	'https://github.com/stevearc/oil.nvim',
+	'https://github.com/brenoprata10/nvim-highlight-colors',
+}
+
+-- colorscheme
+require('nvim-highlight-colors').setup({
+    render = 'virtual',
+})
+require('hybrid').setup({
+    transparent = true,
+})
+require('kanagawa').setup({
+    transparent = true,
+})
+vim.g.have_nerd_font = true 
+vim.opt.termguicolors = true
+vim.cmd.colorscheme('kanagawa-dragon')
+
+-- LSP
+vim.lsp.enable('basedpyright')
+vim.lsp.config('basedpyright', {
+    settings = {
+        ['basedpyright'] = {
+            typeCheckingMode = 'basic',
+        },
+    },
+})
+
+-- File Browsing
+require('oil').setup({
+    columns = {
+        { 'mtime', highlight = 'NonText' },
+    },
+    delete_to_trash = true,
+    view_options = {
+        show_hidden = true
+    },
+    float = {
+        max_width = 0.9,
+        max_height = 0.9,
+        preview_split = 'right'
+    },
+})
+
+
